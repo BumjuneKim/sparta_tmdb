cba18a4d92996a838612a657264272cfb514210c
Jeff <kbj@zigbang.com>
2023-07-03 21:27:52 +0900

    adjust prettier

--- routes/pages.js
+++ routes/pages.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /**
  * 각 페이지에 필요한 정보가 있으면 이곳에서 셋팅해서 넘겨주자
  */
 
-router.route('/')
-  .get((req, res) => {
-    return res.render('pages/index.html');
-  });
+router.route("/").get((req, res) => {
+  return res.render("pages/index.html");
+});
 
 module.exports = router;
